1b3793d35f11698f680810407aafc17ee4d52c14
Joseph Safouri <jsafouri3@gatech.edu>
2024-06-10 21:37:39 -0400

    Character Counter Modification
    I made the Word Counter and a line counter.

--- src/CharCounter/CharCounter.tsx
+++ src/CharCounter/CharCounter.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { text } from "stream/consumers";
 
 /**
  * A React functional component that provides a character counter.
@@ -12,12 +13,25 @@ import React, { useState } from "react";
  */
 function CharCounter() {
   const [textBoxContent, setTextBoxContent] = useState("");
-
+  const countWords = (text: String) => {
+    return text
+      .trim()
+      .split(/\s+/)
+      .filter((word) => word.length > 0).length;
+  };
   return (
     <div>
       <div>
         <span>
           Character Counter: <span>{textBoxContent.length}</span>
+          <br />
+          <span>
+            <span>
+              Word Counter: <span>{countWords(textBoxContent)}</span>
+            </span>
+            <br />
+            Lines: <span>{textBoxContent.split("\n").length}</span>
+          </span>
         </span>
       </div>
 
