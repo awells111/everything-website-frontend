a9658660a98f07cb6676ed0f018601386f6d0579
Joseph Safouri <jsafouri3@gatech.edu>
2024-08-11 13:48:05 -0400

    Added a moving gif as the cursor

--- src/TabbedInterface/Pokemon/Pokemon.tsx
+++ src/TabbedInterface/Pokemon/Pokemon.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Pokemon.css";
 import PokemonByType from "./PokemonByType/PokemonByType";
+import { motion } from "framer-motion";
+
 
 const Pokemon: React.FC = () => {
+  const [mousePosition, setMousePosition] = React.useState({
+    x: 0,
+    y: 0
+  });
+  console.log(mousePosition);
+  useEffect(() => {
+    const mouseMove = (e: { clientX: any; clientY: any; }) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY
+      })
+    }
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  })
+  const variants = {
+    default: {
+      x: mousePosition.x - 16,
+      y: mousePosition.y - 16,
+      transition: {
+        type: "smooth",
+        duration: 0,
+      },
+    },
+  }
   return (
-    <div>
+    <div className="pokemon-tab">
       <PokemonByType />
+      <motion.div
+        className="cursor"
+        variants={variants}
+        animate= "default"
+      />
     </div>
   );
 };
